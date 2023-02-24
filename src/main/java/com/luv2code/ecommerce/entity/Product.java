2dcbe8836db78253d1a67659d96dbead316beb91
Archie Sua <archiesua@gmail.com>
2023-02-24 15:10:10 +1000

    Intial commit for project

---
+++ src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -0,0 +1,14 @@
+package com.luv2code.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Entity
+@Table
+@Data
+public class Product {
+
+
+}
